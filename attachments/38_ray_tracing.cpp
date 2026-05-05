326b1c584a42970158fa72f6e7b7ca0341c04702
Alfa Suni <c4072838@newcastle.ac.uk>
2026-05-05 15:34:51 +0100

    Some settings.

--- attachments/38_ray_tracing.cpp
+++ attachments/38_ray_tracing.cpp
@@ -11,7 +11,7 @@
 #include <vector>
 
 #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
-#	include <vulkan/vulkan_raii.hpp>
+#include <vulkan/vulkan_raii.hpp>
 #else
 import vulkan_hpp;
 #endif
